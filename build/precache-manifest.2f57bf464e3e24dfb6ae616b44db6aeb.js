a7682f6f68c59f1a615e7b35c936fbb63086ef8a
khoand10 <khoand1011@gmail.com>
2020-02-08 16:21:53 +0700

    add react app

---
+++ build/precache-manifest.2f57bf464e3e24dfb6ae616b44db6aeb.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e061fd5796c5796c20a279a3c70e8a6f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f84f7e1ca80aa7d0f889",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "163dd15cca523a63d899",
+    "url": "/static/js/2.970da5d1.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/static/js/2.970da5d1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f84f7e1ca80aa7d0f889",
+    "url": "/static/js/main.179e0029.chunk.js"
+  },
+  {
+    "revision": "5d93c6b2d15332364552",
+    "url": "/static/js/runtime-main.cea588d5.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
--- server.js
+++ server.js
@@ -2,6 +2,17 @@ const express = require('express')
 const app = express()
 const port = process.env.PORT || 3000;
 
-app.get('/', (req, res) => res.send('Hello World!'))
+// app.get('/', (req, res) => res.send('Hello World!'))
+
+app.use(express.static(path.join(__dirname, 'build')));
+
+
+app.get('/*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
